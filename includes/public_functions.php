13fedb48e4704f0123ce84f1211b16022378923f
Milla2000 <millajesso2000@gmail.com>
2023-06-13 00:43:55 +0300

    retrieving posts topic using post-topic relationship table

--- includes/public_functions.php
+++ includes/public_functions.php
@@ -7,12 +7,26 @@ function getPublishedPosts() {
 	global $conn;
 	$sql = "SELECT * FROM posts WHERE published=true";
 	$result = mysqli_query($conn, $sql);
-
 	// fetch all posts as an associative array called $posts
 	$posts = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
-	return $posts;
+	$final_posts = array();
+	foreach ($posts as $post) {
+		$post['topic'] = getPostTopic($post['id']); 
+		array_push($final_posts, $post);
+	}
+	return $final_posts;
 }
-
-// more functions to come here ...
+/* * * * * * * * * * * * * * *
+* Receives a post id and
+* Returns topic of the post
+* * * * * * * * * * * * * * */
+function getPostTopic($post_id){
+	global $conn;
+	$sql = "SELECT * FROM topics WHERE id=
+			(SELECT topic_id FROM post_topic WHERE post_id=$post_id) LIMIT 1";
+	$result = mysqli_query($conn, $sql);
+	$topic = mysqli_fetch_assoc($result);
+	return $topic;
+}
 ?>
